d1e55a743f8678b60169a1e974e7e3ab8a98b74c
ahmadmohey <ahmadmohey@gmail.com>
2018-01-02 18:03:33 +0200

    Complete C# - AssignmentNo20 Solution

---
+++ AssignmentNo20/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AssignmentNo20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            SayHi hi = new SayHi(); string name = "Ahmad"; int count = 2;
+            List<Thread> listofThreads = new List<Thread>();
+
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiEnglish(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiKorean(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiFrench(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiGreek(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiHindi(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiRussian(name, count))));
+            listofThreads.Add(new Thread(new ThreadStart(() => hi.SayHiSwedish(name, count))));
+
+            foreach (var thread in listofThreads)
+            {
+                thread.Start();
+            }
+        }
+    }
+
+    class SayHi
+    {
+        public void SayHiEnglish(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Hi " + name);
+            }
+        }
+
+        public void SayHiKorean(string name,int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Annyeong " + name);
+            }
+        }
+        public void SayHiFrench(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Salut " + name);
+            }
+        }
+
+        public void SayHiGreek(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Geia " + name);
+            }            
+        }
+
+        public void SayHiHindi(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Namaste " + name);
+            }
+        }
+
+        public void SayHiRussian(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Zdravstvuy " + name);
+            }            
+        }
+
+        public void SayHiSwedish(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Hej " + name);
+            }
+        }
+    }
+}
